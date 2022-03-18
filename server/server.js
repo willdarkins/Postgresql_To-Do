7de2c6f841f5d5ad832fe04841d19010700c6e74
Will Darkins <willdarkins@gmail.com>
2022-03-17 21:00:18 -0700

    added middleware of cors to recieve body object from client side... now building database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 //listen call to port
 app.listen(port, () => {
