42f39ecb0a496165b9624b41c29bd110b66acd27
Will Darkins <willdarkins@gmail.com>
2022-03-17 22:18:51 -0700

    created get route to retrieve one todo by id

--- server/server.js
+++ server/server.js
@@ -37,7 +37,17 @@ app.get("/todos", async(req, res) => {
 })
 
 //get a todo
-
+app.get("/todos/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query(
+            "SELECT * FROM todo WHERE todo_id = $1",
+            [id])
+            res.json(todo.rows[0])
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 //update todo
 
 //delete todo
